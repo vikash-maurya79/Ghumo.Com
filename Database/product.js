bb1155249927d70ed1ba86bf613f73047646c8a1
Vikash maurya <vikashm7973@gmail.com>
2025-06-15 21:51:44 +0530

    Created Review Schema

--- Database/product.js
+++ Database/product.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
-    descreption:String,
-    url:String,
-    address:String,
-    address2:String,
-    city:String,
-    state:String,
-    pincode:Number,
+    descreption: String,
+    url: String,
+    address: String,
+    address2: String,
+    city: String,
+    state: String,
+    pincode: Number,
+    reviews: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Review"
+        }
+    ]
 
 })
-const product_data = mongoose.model("product_data",productSchema);
-module.exports=product_data;
+const product_data = mongoose.model("product_data", productSchema);
+module.exports = product_data;
